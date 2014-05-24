bf096a7f0c69496166fc9e95d9fda81d7b85f4b0
Michal Matlega <m1sz3lek@gmail.com>
2014-05-24 22:24:08 +0200

    Losowanie liczb i przesuwanie/scalanie blokow

---
+++ 2048mp/src/model/Block.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Block {
+	int val;
+	boolean adv = false;
+	public Block(int val) {
+		super();
+		this.val = val;
+	}
+	
+	
+}
--- 2048mp/src/model/Board.java
+++ 2048mp/src/model/Board.java
@@ -4,72 +4,82 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JPanel;
 
 public class Board extends JPanel implements KeyListener {
-	ArrayList<ArrayList<Integer>> board = new ArrayList<ArrayList<Integer>>();
+	ArrayList<ArrayList<Block>> board = new ArrayList<ArrayList<Block>>();
 	ArrayList<Position> freeTiles = new ArrayList<Position>();
 	int size = 4;
 
 	public Board() {
 		super();
 
-		 addKeyListener(this);
-		 setFocusable(true); //IMPORTANT!!!
+		addKeyListener(this);
+		setFocusable(true); // IMPORTANT!!!
 
 		for (int i = 0; i < size; i++) {
-			board.add(new ArrayList<Integer>());
+			board.add(new ArrayList<Block>());
 		}
 
 		for (int i = 0; i < board.size(); i++)
 			for (int j = 0; j < size; j++)
-				board.get(i).add(0);
+				board.get(i).add(new Block(0));
 	}
 
 	void reset() {
 		for (int i = 0; i < board.size(); i++)
 			for (int j = 0; j < board.get(i).size(); j++)
-				board.get(i).set(j, 0);
+				board.get(i).get(j).val = 0;
 	}
-	
-	void updateFreeTiles(){
+
+	void updateFreeTiles() {
 		freeTiles.clear();
-		
-		for(int i = 0; i < board.size(); i++)
-			for(int j = 0; j < board.size(); j++){
-				if(board.get(i).get(j) == 0){
-					freeTiles.add(new Position(i,j));
+
+		for (int i = 0; i < board.size(); i++)
+			for (int j = 0; j < board.size(); j++) {
+				if (board.get(i).get(j).val == 0) {
+					freeTiles.add(new Position(i, j));
 				}
 			}
+	}
+	
+	void clearAdv(){
+		for(int i = 0; i < board.size(); i++){
+			for(int j = 0; j < board.size(); j++){
+				board.get(i).get(j).adv = false;
+			}
+		}
 	}
 
 	void moveUp() {
 		boolean moved = true;
+		
+		clearAdv();
 
 		while (moved) {
 			moved = false;
 
 			for (int i = 1; i < board.size(); i++) {
 				for (int j = 0; j < board.size(); j++) {
-					if (board.get(i).get(j) != 0) {
-						Integer neigh = board.get(i - 1).get(j);
-						Integer target = board.get(i).get(j);
+					if (board.get(i).get(j).val != 0 && !board.get(i).get(j).adv) {
+						Integer neigh = board.get(i - 1).get(j).val;
+						Integer target = board.get(i).get(j).val;
 
 						if (neigh == target) {
-							board.get(i - 1).set(j, neigh * 2);
-							board.get(i).set(j, 0);
+							board.get(i - 1).get(j).val = neigh * 2;
+							board.get(i - 1).get(j).adv = true;
+							board.get(i).get(j).val = 0;
 							moved = true;
-							// System.out.println("move1");
 							continue;
 
 						}
 
 						if (neigh == 0) {
-							board.get(i - 1).set(j, target);
-							board.get(i).set(j, 0);
+							board.get(i - 1).get(j).val = target;
+							board.get(i).get(j).val = 0;
 							moved = true;
-							// System.out.println("move2");
 						}
 					}
 				}
@@ -79,26 +89,28 @@ public class Board extends JPanel implements KeyListener {
 
 	void moveDown() {
 		boolean moved = true;
+		clearAdv();
 
 		while (moved) {
 			moved = false;
 
 			for (int i = board.size() - 2; i >= 0; i--) {
 				for (int j = 0; j < board.size(); j++) {
-					if (board.get(i).get(j) != 0) {
-						Integer neigh = board.get(i + 1).get(j);
-						Integer target = board.get(i).get(j);
+					if (board.get(i).get(j).val != 0 && !board.get(i).get(j).adv) {
+						Integer neigh = board.get(i + 1).get(j).val;
+						Integer target = board.get(i).get(j).val;
 
 						if (neigh == target) {
-							board.get(i + 1).set(j, neigh * 2);
-							board.get(i).set(j, 0);
+							board.get(i + 1).get(j).val = neigh * 2;
+							board.get(i + 1).get(j).adv = true;
+							board.get(i).get(j).val = 0;
 							moved = true;
 							continue;
 						}
 
 						if (neigh == 0) {
-							board.get(i + 1).set(j, target);
-							board.get(i).set(j, 0);
+							board.get(i + 1).get(j).val = target;
+							board.get(i).get(j).val = 0;
 							moved = true;
 						}
 					}
@@ -109,26 +121,28 @@ public class Board extends JPanel implements KeyListener {
 
 	void moveLeft() {
 		boolean moved = true;
+		clearAdv();
 
 		while (moved) {
 			moved = false;
-			
-				for (int j = 1; j < board.size(); j++) {
+
+			for (int j = 1; j < board.size(); j++) {
 				for (int i = 0; i < board.size(); i++) {
-					if (board.get(i).get(j) != 0) {
-						Integer neigh = board.get(i).get(j - 1);
-						Integer target = board.get(i).get(j);
+					if (board.get(i).get(j).val != 0 && !board.get(i).get(j).adv) {
+						Integer neigh = board.get(i).get(j - 1).val;
+						Integer target = board.get(i).get(j).val;
 
 						if (neigh == target) {
-							board.get(i).set(j - 1, neigh * 2);
-							board.get(i).set(j, 0);
+							board.get(i).get(j - 1).val = neigh * 2;
+							board.get(i).get(j - 1).adv = true;
+							board.get(i).get(j).val = 0;
 							moved = true;
 							continue;
 						}
 
 						if (neigh == 0) {
-							board.get(i).set(j - 1, target);
-							board.get(i).set(j, 0);
+							board.get(i).get(j - 1).val = target;
+							board.get(i).get(j).val = 0;
 							moved = true;
 						}
 					}
@@ -139,25 +153,27 @@ public class Board extends JPanel implements KeyListener {
 
 	void moveRight() {
 		boolean moved = true;
+		clearAdv();
 
 		while (moved) {
 			moved = false;
-			for (int j = board.size() - 2; j > 0; j--) {
+			for (int j = board.size() - 2; j >= 0; j--) {
 				for (int i = 0; i < board.size(); i++) {
-					if (board.get(i).get(j) != 0) {
-						Integer neigh = board.get(i).get(j + 1);
-						Integer target = board.get(i).get(j);
+					if (board.get(i).get(j).val != 0 && !board.get(i).get(j).adv) {
+						Integer neigh = board.get(i).get(j + 1).val;
+						Integer target = board.get(i).get(j).val;
 
 						if (neigh == target) {
-							board.get(i).set(j + 1, neigh * 2);
-							board.get(i).set(j, 0);
+							board.get(i).get(j + 1).val = neigh * 2;
+							board.get(i).get(j + 1).adv = true;
+							board.get(i).get(j).val = 0;
 							moved = true;
 							continue;
 						}
 
 						if (neigh == 0) {
-							board.get(i).set(j + 1, target);
-							board.get(i).set(j, 0);
+							board.get(i).get(j + 1).val = target;
+							board.get(i).get(j).val = 0;
 							moved = true;
 						}
 					}
@@ -166,36 +182,36 @@ public class Board extends JPanel implements KeyListener {
 		}
 	}
 
-	void spawn(int x, int y, int val) {
-		board.get(x).set(y, val);
-	}
-	
-	void randSpawn(){
+//	void spawn(int x, int y, int val) {
+//		board.get(x).set(y, val);
+//	}
+
+	void randSpawn() {
 		updateFreeTiles();
-		
-		if(freeTiles.isEmpty()){
+
+		if (freeTiles.isEmpty()) {
 			return;
 		}
-		
-		int number = new Random().nextInt(2);	//2 or 4
-		
-		
+
+		int number = new Random().nextInt(2); // 2 or 4
+
 		Position tile = freeTiles.get(new Random().nextInt(freeTiles.size()));
-		
-		if(number == 0){
-			board.get(tile.x).set(tile.y, 2);
-		}else{
-			board.get(tile.x).set(tile.y, 4);
+
+		if (number == 0) {
+			board.get(tile.x).get(tile.y).val = 2;
+		} else {
+			board.get(tile.x).get(tile.y).val = 4;
 		}
-		
+
 	}
 
 	void showBoard() {
 		for (int i = 0; i < board.size(); i++) {
 			for (int j = 0; j < board.size(); j++) {
-				System.out.print(board.get(i).get(j));
+				System.out.print(board.get(i).get(j).val + "\t");
 			}
 
+			System.out.println();
 			System.out.println();
 		}
 
@@ -206,45 +222,56 @@ public class Board extends JPanel implements KeyListener {
 	public static void main(String[] args) {
 		Board board = new Board();
 
-		//board.spawn(2, 2, 2);
-		//board.spawn(3, 2, 4);
-		//board.spawn(0, 0, 4);
-		//board.show();
-		//board.moveLeft();
-	//	board.moveDown();
-	//	board.move();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-//		board.randSpawn();
-		board.showBoard();
+		// board.spawn(2, 2, 2);
+		// board.spawn(3, 2, 4);
+		// board.spawn(0, 0, 4);
+		// board.show();
+		// board.moveLeft();
+		// board.moveDown();
+		// board.move();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+		// board.randSpawn();
+
+		// board.showBoard();
+
+		Scanner read = new Scanner(System.in);
+
+		String decision;
 		
-//		Scanner read = new Scanner(System.in); //obiekt do odebrania danych od użytkownika
-//		 
-//	    
-//	    String decision;
-		
-//		while(true){
-//			board.spawn(new Random().nextInt(3), new Random().nextInt(3), 2);
-//			board.show();
-//			decision = read.nextLine();
-//			if(decision == "w"){
-//				board.moveUp();
-//			}
-//		}
+		while (true) {
+			board.randSpawn();
+			board.showBoard();
+			decision = read.nextLine();
+			switch (decision) {
+			case "w":
+				board.moveUp();
+				break;
+			case "s":
+				board.moveDown();
+				break;
+			case "a":
+				board.moveLeft();
+				break;
+			case "d":
+				board.moveRight();
+				break;
+			}
+		}
 
 	}
 
@@ -257,20 +284,30 @@ public class Board extends JPanel implements KeyListener {
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 
-		if (arg0.getKeyCode() == KeyEvent.VK_W)
+		if (arg0.getKeyCode() == KeyEvent.VK_W) {
 			moveUp();
+			showBoard();
+		}
 
-		if (arg0.getKeyCode() == KeyEvent.VK_S)
+		if (arg0.getKeyCode() == KeyEvent.VK_S) {
 			moveDown();
+			showBoard();
+		}
 
-		if (arg0.getKeyCode() == KeyEvent.VK_A)
+		if (arg0.getKeyCode() == KeyEvent.VK_A) {
 			moveLeft();
+			showBoard();
+		}
 
-		if (arg0.getKeyCode() == KeyEvent.VK_D)
+		if (arg0.getKeyCode() == KeyEvent.VK_D) {
 			moveRight();
+			showBoard();
+		}
 
-		if (arg0.getKeyCode() == KeyEvent.VK_ENTER)
+		if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
 			reset();
+			showBoard();
+		}
 
 	}
 
