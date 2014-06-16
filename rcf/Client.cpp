04fc6f9e77935f2dd227083e5337f875c19f3606
Michal Matlega <m1sz3lek@gmail.com>
2014-06-17 01:08:05 +0200

    Sekwencyjna wymiana ruchow

--- rcf/Client.cpp
+++ rcf/Client.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 #include <RCF/RCF.hpp>
+#include <cstdlib>
+#include <vector>
+#include <SF/vector.hpp>
 
 using namespace std;
 
@@ -10,18 +13,180 @@ RCF_BEGIN(I_HelloWorld, "I_HelloWorld")
     RCF_METHOD_V0(void, dec)
 RCF_END(I_HelloWorld)
 
+RCF_BEGIN(I_Board, "I_Board")
+    RCF_METHOD_V1(void, turn, const int &)
+    RCF_METHOD_R0(vector< vector<double> >, getBoard)
+    RCF_METHOD_R1(int, addPlayer, const string &)
+    RCF_METHOD_V1(void, removePlayer, const int &)
+    RCF_METHOD_R1(bool, checkMyTurn, const int &)
+RCF_END(I_Board)
+
+class Position {
+    
+    public:
+        int x;
+        int y;
+        
+        Position(int x, int y) {
+            this->x = x;
+            this->y = y;
+        }
+    
+};
+
+class Block {
+    
+    public:
+        int val;
+        bool adv;
+        Block(int val){
+            this->val = val;
+            this->adv = false;
+        }
+};
+
+class Player{
+    public:
+        int id;
+        string nick;
+        int score;
+
+        Player(string _nick){
+            this->nick = _nick;
+            score = 0;
+        }
+
+        void setId(int val){
+            this->id = val;
+        }
+};
+
+class ClientBoard{
+    public:
+
+    int size;
+
+    vector< vector<Block> > board;
+    vector<Position> freeTiles;
+
+    ClientBoard(){
+
+        this->size = 4;
+
+        for (int i = 0; i < size; i++) {
+            vector<Block> row;
+            
+            for(int j = 0; j < size; j++){
+                Block block(0);
+                row.push_back(block);
+            }
+
+            board.push_back(row);
+
+        }
+    }
+
+    void showBoard() {
+        printf("\033[2J\033[1;1H");
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                //System.out.print(board.get(i).get(j).val + "\t");
+                if(board[i][j].val != 0){
+                    cout << board[i][j].val << "\t";
+                }else{
+                    cout << "." << "\t";
+                }
+            }
+
+            cout << endl;
+        }
+
+        //cout << "--------------------" << endl;
+
+        
+    }
+
+    vector< vector<double> > getBoard(){
+        
+        vector< vector<double> > res;
+
+        for(int i = 0; i < board.size(); i++){
+            vector<double> row;
+            for(int j = 0; j < board.size(); j++){
+                row.push_back(board[i][j].val);
+            }
+            res.push_back(row);
+        }
+        
+        return res;
+    }
+
+    void updateBoard(RcfClient<I_Board> _client){
+        vector< vector<double> > newBoard = _client.getBoard(); 
+
+        for(int i = 0; i < board.size(); i++)
+            for(int j = 0; j < board.size(); j++)
+                this->board[i][j].val = newBoard[i][j];
+    }
+
+};
+
 int main()
 {
     RCF::RcfInitDeinit rcfInit;
 
-    string input;
+    string nick = "Anonim";
 
-    std::cout << "Calling the I_HelloWorld Print() method." << std::endl;
-    RcfClient<I_HelloWorld> client( RCF::TcpEndpoint(50001) );
+    //std::cout << "Calling the I_HelloWorld Print() method." << std::endl;
+    //RcfClient<I_HelloWorld> client( RCF::TcpEndpoint(50001) );
+    RcfClient<I_Board> client( RCF::TcpEndpoint(50001) );
 
+    Player myself(nick);
+
+    myself.setId(client.addPlayer(nick));
+
+    ClientBoard board;
+
+    char decision;
+    bool quit = false;
+        
+    while (!quit) {
+        //board.updateBoard(client);
+        //board.showBoard();
+        
+        while(!client.checkMyTurn(myself.id)){
+            RCF::sleepMs(500);
+            board.updateBoard(client);
+            board.showBoard();  
+        }
+
+        board.updateBoard(client);
+        board.showBoard();
+
+        cin >> decision;
+        switch (decision) {
+        case 'w':
+            client.turn(0);
+            break;
+        case 's':
+            client.turn(2);
+            break;
+        case 'a':
+            client.turn(3);
+            break;
+        case 'd':
+            client.turn(1);
+            break;
+        case 'q':
+            quit = true;
+        }
+    }
+
+    client.removePlayer(myself.id);
     
 
-    bool quit = false;
+    /*bool quit = false;
 
     client.inc();
     client.Print("Player join the server");
@@ -38,7 +203,7 @@ int main()
     	client.Print(input);
     }
 
-    client.dec();
+    client.dec();*/
 
     return 0;
 }
--- rcf/Server.cpp
+++ rcf/Server.cpp
@@ -1,15 +1,25 @@
 #include <RCF/RCF.hpp>
 
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <SF/vector.hpp>
 
 using namespace std;
-
 
 RCF_BEGIN(I_HelloWorld, "I_HelloWorld")
     RCF_METHOD_V1(void, Print, const std::string &)
     RCF_METHOD_V0(void, inc)
     RCF_METHOD_V0(void, dec)
 RCF_END(I_HelloWorld)
+
+RCF_BEGIN(I_Board, "I_Board")
+    RCF_METHOD_V1(void, turn, const int &)
+    RCF_METHOD_R0(vector< vector<double> >, getBoard)
+    RCF_METHOD_R1(int, addPlayer, const string &)
+    RCF_METHOD_V1(void, removePlayer, const int &)
+    RCF_METHOD_R1(bool, checkMyTurn, const int &)
+RCF_END(I_Board)
 
 class HelloWorldImpl
 {
@@ -34,20 +44,420 @@ public:
     }
 };
 
+class Position {
+    
+    public:
+        int x;
+        int y;
+        
+        Position(int x, int y) {
+            this->x = x;
+            this->y = y;
+        }
+    
+};
+
+class Block {
+    
+    public:
+        int val;
+        bool adv;
+        Block(int val){
+            this->val = val;
+            this->adv = false;
+        }
+};
+
+class Player{
+    public:
+        int id;
+        string nick;
+        int score;
+
+        Player(string _nick){
+            this->nick = _nick;
+            score = 0;
+        }
+
+        void setId(int val){
+            this->id = val;
+        }
+};
+
+class Board{
+    public:
+
+    int size;
+
+    int currplayer;
+    int maxplayers;
+
+    int idcounter;
+
+    vector< vector<Block> > board;
+    vector<Position> freeTiles;
+
+    vector<Player> players;
+    vector<Player> spectators;
+
+    Board(){
+        srand(time(NULL));
+        this->size = 4;
+
+        this->currplayer = 0;
+        this->maxplayers = 4;
+        this->idcounter = 0;
+
+        for (int i = 0; i < size; i++) {
+            vector<Block> row;
+            
+            for(int j = 0; j < size; j++){
+                Block block(0);
+                row.push_back(block);
+            }
+
+            board.push_back(row);
+
+        }
+
+        randSpawn();
+    }
+
+    int addPlayer(const string _nick){
+        Player newPlayer(_nick);
+        
+        if(players.size() <= 4){
+            newPlayer.setId(idcounter);
+            idcounter++;
+            players.push_back(newPlayer);
+            cout << newPlayer.nick << " dolacza do gry" << endl;
+        }
+        else{
+            newPlayer.setId(-1);
+            cout << newPlayer.nick << " dolacza jako widz" << endl;
+        }
+        
+        return newPlayer.id;
+    }
+
+    void removePlayer(const int _id){
+        for(int i = 0; i < players.size(); i++){
+            if(players[i].id == _id){
+                cout << players[i].nick << " opuszcza gre" << endl;
+                players.erase(players.begin() + i);
+            }
+        }
+    }
+
+    void switchPlayer(){
+        currplayer++;
+
+        if(currplayer == players.size()){
+            currplayer = 0;
+        }
+    }
+
+    bool checkMyTurn(int _id){
+        int currid = players[currplayer].id;
+
+        cout << currid << " " << _id << endl;
+
+        if(_id == currid){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void reset() {
+        for (int i = 0; i < board.size(); i++)
+            for (int j = 0; j < board[i].size(); j++)
+                board[i][j].val = 0;
+
+        randSpawn();
+    }
+
+    void updateFreeTiles() {
+        freeTiles.clear();
+
+        for (int i = 0; i < board.size(); i++)
+            for (int j = 0; j < board.size(); j++) {
+                if (board[i][j].val == 0) {
+                    
+                    Position pos(i,j);
+                    freeTiles.push_back(pos);
+                }
+            }
+    }
+    
+    void clearAdv(){
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board.size(); j++){
+                board[i][j].adv = false;
+            }
+        }
+    }
+
+    bool moveUp() {
+        bool moved = true;
+        bool everMoved = false;
+        
+        clearAdv();
+
+        while (moved) {
+            moved = false;
+
+            for (int i = 1; i < board.size(); i++) {
+                for (int j = 0; j < board.size(); j++) {
+                    if (board[i][j].val != 0 && !board[i][j].adv) {
+                        //int neigh = board.get(i - 1).get(j).val;
+                        //int target = board.get(i).get(j).val;
+
+                        int neigh = board[i-1][j].val;
+                        int target = board[i][j].val;
+
+                        if (neigh == target) {
+                            //board.get(i - 1).get(j).val = neigh * 2;
+                            //board.get(i - 1).get(j).adv = true;
+                            //board.get(i).get(j).val = 0;
+                            
+                            board[i-1][j].val = neigh * 2;
+                            board[i-1][j].adv = true;
+                            board[i][j].val = 0;
+
+                            moved = true;
+                            everMoved = true;
+                            continue;
+
+                        }
+
+                        if (neigh == 0) {
+                            //board.get(i - 1).get(j).val = target;
+                            //board.get(i).get(j).val = 0;
+
+                            board[i-1][j].val = target;
+                            board[i][j].val = 0;
+                            moved = true;
+                            everMoved = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return everMoved;
+    }
+
+    bool moveDown() {
+        bool moved = true;
+        bool everMoved = false;
+
+        clearAdv();
+
+        while (moved) {
+            moved = false;
+
+            for (int i = board.size() - 2; i >= 0; i--) {
+                for (int j = 0; j < board.size(); j++) {
+                    if (board[i][j].val != 0 && !board[i][j].adv) {
+                        int neigh = board[i+1][j].val;
+                        int target = board[i][j].val;
+
+                        if (neigh == target) {
+                            board[i+1][j].val = neigh * 2;
+                            board[i+1][j].adv = true;
+                            board[i][j].val = 0;
+                            moved = true;
+                            everMoved = true;
+                            continue;
+                        }
+
+                        if (neigh == 0) {
+                            board[i+1][j].val = target;
+                            board[i][j].val = 0;
+                            moved = true;
+                            everMoved = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return everMoved;
+    }
+
+    bool moveLeft() {
+        bool moved = true;
+        bool everMoved = false;
+        clearAdv();
+
+        while (moved) {
+            moved = false;
+
+            for (int j = 1; j < board.size(); j++) {
+                for (int i = 0; i < board.size(); i++) {
+                    if (board[i][j].val != 0 && !board[i][j].adv) {
+                        int neigh = board[i][j-1].val;
+                        int target = board[i][j].val;
+
+                        if (neigh == target) {
+                            board[i][j-1].val = neigh * 2;
+                            board[i][j-1].adv = true;
+                            board[i][j].val = 0;
+                            moved = true;
+                            everMoved = true;
+                            continue;
+                        }
+
+                        if (neigh == 0) {
+                            board[i][j-1].val = target;
+                            board[i][j].val = 0;
+                            moved = true;
+                            everMoved = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return everMoved;
+    }
+
+    bool moveRight() {
+        bool moved = true;
+        bool everMoved = false;
+        clearAdv();
+
+        while (moved) {
+            moved = false;
+            for (int j = board.size() - 2; j >= 0; j--) {
+                for (int i = 0; i < board.size(); i++) {
+                    if (board[i][j].val != 0 && !board[i][j].adv) {
+                        int neigh = board[i][j+1].val;
+                        int target = board[i][j].val;
+
+                        if (neigh == target) {
+                            board[i][j+1].val = neigh * 2;
+                            board[i][j+1].adv = true;
+                            board[i][j].val = 0;
+                            moved = true;
+                            everMoved = true;
+                            continue;
+                        }
+
+                        if (neigh == 0) {
+                            board[i][j+1].val = target;
+                            board[i][j].val = 0;
+                            moved = true;
+                            everMoved = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return everMoved;
+    }
+
+//  void spawn(int x, int y, int val) {
+//      board.get(x).set(y, val);
+//  }
+
+    bool move(int dir){
+        switch(dir){
+            case 0: return moveUp();
+            case 1: return moveRight();
+            case 2: return moveDown();
+            case 3: return moveLeft();
+        }
+
+        return false;
+    }
+
+    void turn(const int dir){
+        if(move(dir)){
+            randSpawn();
+            switchPlayer();
+        }
+
+    }
+
+    void randSpawn() {
+        updateFreeTiles();
+
+        if (freeTiles.empty()) {
+            return;
+        }
+
+        //int number = new Random().nextInt(2); // 2 or 4
+
+        int number = rand() % 2;
+
+        //Position tile = freeTiles.get(new Random().nextInt(freeTiles.size()));
+
+        Position tile = freeTiles[rand()%freeTiles.size()];
+
+        if (number == 0) {
+            board[tile.x][tile.y].val = 2;
+        } else {
+            board[tile.x][tile.y].val = 4;
+        }
+
+    }
+
+    void showBoard() {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                //System.out.print(board.get(i).get(j).val + "\t");
+                if(board[i][j].val != 0){
+                    cout << board[i][j].val << "\t";
+                }else{
+                    cout << "." << "\t";
+                }
+            }
+
+            cout << endl;
+        }
+
+        cout << "--------------------" << endl;
+        
+    }
+
+    vector< vector<double> > getBoard(){
+        
+        vector< vector<double> > res;
+
+        for(int i = 0; i < board.size(); i++){
+            vector<double> row;
+            for(int j = 0; j < board.size(); j++){
+                row.push_back(board[i][j].val);
+            }
+            res.push_back(row);
+        }
+        
+        return res;
+    }
+
+};
+
 
 int main()
 {
+
     RCF::RcfInitDeinit rcfInit;
 
-    HelloWorldImpl helloWorld;
+    //HelloWorldImpl helloWorld;
+    Board board;
     RCF::RcfServer server( RCF::TcpEndpoint(50001) );
-    server.bind<I_HelloWorld>(helloWorld);
+    //server.bind<I_HelloWorld>(helloWorld);
+    server.bind<I_Board>(board);
+
     server.start();
     
-    int input;
-    bool quit;
+    //int input;
+    //bool quit;
 
-    while(true){
+   /* while(true){
         cin >> input;
         switch(input){
             case 1: helloWorld.inc();break;
@@ -58,10 +468,11 @@ int main()
         if(quit){
             break;
         }
-    }
+    }*/
 
     //std::cout << "Press Enter to exit..." << std::endl;
-    //std::cin.get();
+    //board.showBoard();
+    std::cin.get();
 
     return 0;
 }
